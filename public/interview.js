4faa861422be3243215d06b246abc5e2cef43097
jerry5789k1 <angeryboy71272@gmail.com>
2019-04-15 00:13:43 +0800

    rewrite Q3

--- public/interview.js
+++ public/interview.js
@@ -25,10 +25,8 @@ const mergeInterval = (...args) => (interval) => {
     for (let i = 0; i < args.length; i++) {
         if (args[i][0] > interval[0]) {
             newIntervalList.splice(i, 0, interval);
-            break
         }
     }
-    console.log(newIntervalList)
     for (let i = 0; i < newIntervalList.length; i++) {
         if (result.length === 0 || result[result.length - 1][1] < newIntervalList[i][0]) {
             result.push(newIntervalList[i])
@@ -43,31 +41,28 @@ const mergeInterval = (...args) => (interval) => {
 console.log(mergeInterval([1, 2], [3, 5], [6, 7], [8, 10], [12, 16])([4, 9]))
 
 //is in grid 
-const isInGrid = (str = '') => {
+const isInGrid = (grid) => (str = '') => {
     const strToArray = [...str]
-    const grid = [
-        ['A', 'B', 'C', 'E'],
-        ['S', 'F', 'C', 'S'],
-        ['A', 'D', 'E', 'E']
-    ]
-    const flatGrid = [...grid[0], ...grid[1], ...grid[2]];
-    const table = {
-        A: 0,
-        B: 0,
-        C: 0,
-        D: 0,
-        E: 0,
-        F: 0,
-        S: 0
-    }
+    const flatGrid = grid.reduce((acc,row)=> [...acc,...row],[]);
+    const table = {}
     flatGrid.forEach((word) => {
-        table[word] = table[word] + 1
+        if(table[word] === undefined){
+          table[word] = 1
+        }else {
+           table[word] = table[word] + 1
+        }
     })
     strToArray.forEach((word) => {
         table[word] = table[word] - 1
     })
     return strToArray.every((word) => table[word] >= 0)
 }
+
+console.log(isInGrid( [
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E']
+])('ABCB'))
 
 console.log(isInGrid('ABCB'))
 
@@ -76,9 +71,7 @@ const addTwoNumber = (x, y) => {
     while (y != 0) {
 
         let carry = x & y;
-        console.log(x, y)
         x = x ^ y;
-
         y = carry << 1;
     }
     return x;
