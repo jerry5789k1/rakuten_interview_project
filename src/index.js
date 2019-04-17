34e9b6fe1ece809767bb1e9814edd15539887fb5
jerry5789k1 <angeryboy71272@gmail.com>
2019-04-17 22:23:32 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppContainer from './redux/AppContainer';
-// redux
+import AppContainer from './AppContainer';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import Reducer from './redux/reducer';
+import Reducer from './reducer';
 const store = createStore(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 ReactDOM.render(
    <Provider store={store}> 
